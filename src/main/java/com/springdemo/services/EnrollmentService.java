8447b38283e319dde59f4b6e5a4efc866e7aebd5
monxvoll <stedy3348112688@gmail.com>
2025-09-08 11:21:35 -0500

    feat: add basic services and interface structure

---
+++ src/main/java/com/springdemo/services/EnrollmentService.java
@@ -0,0 +1,13 @@
+package com.springdemo.services;
+
+import com.springdemo.repositories.EnrollmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EnrollmentService {
+    @Autowired
+    private EnrollmentRepository enrollmentRepository;
+
+
+}
